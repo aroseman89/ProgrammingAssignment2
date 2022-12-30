62de4d91f5ccf9888e6f9cfc2774d5564dbd57d6
aroseman89 <aroroseman@gmail.com>
2022-12-30 16:32:50 -0700

    Added explanatory comments

--- Programming Assignment 2 Lexical Scoping.R
+++ Programming Assignment 2 Lexical Scoping.R
@@ -1,26 +1,52 @@
+#Matrix object that can cache inverse
 makeCacheMatrix <- function(x = matrix()) {
+ 
+   #Initialize inverse property
   i <- NULL
+  
+  #Set matrix
   set <- function(y) {
     x <<- y
     i <<- NULL
   }
+  #Get matrix
+  
   get <- function() x
+  
+  #Set inverse of matrix
   setinverse <- function(inverse) i <<- inverse
+  
+  #Get inverse of matrix
   getinverse <- function() i
-  list(set = set,
+ 
+  #List of methods
+   list(set = set,
        get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
+# Compute the inverse of the special matrix returned by "makeCacheMatrix" above. If the inverse has already been calculated (and the matrix has not changed), then the "cachesolve" should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-  i <- x$getinverse()
-  if (!is.null(i)) {
+ 
+  #Return a matrix, inverse of x
+   i <- x$getinverse()
+  
+   #Return inverse if already set
+   if (!is.null(i)) {
     message("getting cached data")
     return(i)
   }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
+ 
+   #Get matrix
+    data <- x$get()
+
+    #Calculate inverse
+      i <- solve(data, ...)
+ 
+      #Set inverse
+       x$setinverse(i)
+ 
+       #Return matrix
+        i
 }
